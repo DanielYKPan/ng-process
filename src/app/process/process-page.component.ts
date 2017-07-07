1913e6e9f9d6f15a8c559c1503d4ecaa1ad95523
Yaokun Pan <myron.yk.pan@gmail.com>
2017-07-07 14:15:18 +1000

    Used angular 4 animation in process-page

--- src/app/process/process-page.component.ts
+++ src/app/process/process-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProcessEvent, ProcessEventType } from './process-event.class';
 import { Subscription } from 'rxjs/Rx';
 import { ProcessService } from './process.service';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'app-process-page',
@@ -14,34 +14,38 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
     styleUrls: ['./process-page.component.scss'],
     animations: [
         trigger('flyInOut', [
-            state('show', style({opacity: 1})),
-            transition('void => show', [
-                style({opacity: 0}),
-                animate('500ms 500ms ease')
+            transition(':enter', [
+                group([
+                    query('.left', [
+                        style({transform: 'translateY(-150%) skew(-10deg) translateZ(0)'}),
+                        animate('650ms ease', style({
+                            transform: 'translateY(0) skew(-10deg) translateZ(0)'
+                        }))
+                    ]),
+                    query('.right', [
+                        style({transform: 'translateY(150%) skew(-10deg) translateZ(0)'}),
+                        animate('650ms ease', style('*'))
+                    ]),
+                    query('.yk-loading-page-loader', [
+                        style({opacity: 0}),
+                        animate('500ms 400ms ease', style({opacity: 1}))
+                    ]),
+                ]),
             ]),
-            transition('show => void', [
-                animate('200ms ease',
-                    style({opacity: 0}))
-            ]),
-        ]),
-        trigger('flyUpDown', [
-            state('up', style({transform: 'translateY(0) skew(-10deg) translateZ(0)'})),
-            state('down', style({transform: 'translateY(0) skew(-10deg) translateZ(0)'})),
-            transition('void => up', [
-                style({transform: 'translateY(150%) skew(-10deg) translateZ(0)'}),
-                animate('650ms ease')
-            ]),
-            transition('up => void', [
-                animate('650ms 200ms ease',
-                    style({transform: 'translateY(150%) skew(-10deg) translateZ(0)'}))
-            ]),
-            transition('void => down', [
-                style({transform: 'translateY(-150%) skew(-10deg) translateZ(0)'}),
-                animate('650ms ease')
-            ]),
-            transition('down => void', [
-                animate('650ms 200ms ease',
-                    style({transform: 'translateY(-150%) skew(-10deg) translateZ(0)'}))
+            transition(':leave', [
+                group([
+                    query('.left', [
+                        animate('650ms 500ms ease',
+                            style({transform: 'translateY(-150%) skew(-10deg) translateZ(0)'}))
+                    ]),
+                    query('.right', [
+                        animate('650ms 500ms ease',
+                            style({transform: 'translateY(150%) skew(-10deg) translateZ(0)'}))
+                    ]),
+                    query('.yk-loading-page-loader', [
+                        animate('500ms ease', style({opacity: 0}))
+                    ]),
+                ]),
             ]),
         ]),
     ]
